9fd5b5e6fc72e94738f5b3ab7c26904dbf81b2e2
kaanyagci <kaan.yagci@makepad.fr>
2021-07-19 01:36:14 +0200

    Brew formula update for concom version v0.0.3

--- concom.rb
+++ concom.rb
@@ -5,29 +5,29 @@
 class Concom < Formula
   desc "An easy solution to use conventional commits from your command line on any kind of project."
   homepage "https://github.com/Makepad-fr/concom"
-  version "0.0.2"
+  version "0.0.3"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.2/concom_0.0.2_Darwin_x86_64.tar.gz"
-      sha256 "9792a1e8f8a61e40e71d4f841d32a05efe7fc4f16f9d7e15cb4e4a74f7312452"
+      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.3/concom_0.0.3_Darwin_x86_64.tar.gz"
+      sha256 "ed7ac439cc42b1f529dab665e8357c0f6a684adf7d33e0d878fe4c628ecc9b3b"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.2/concom_0.0.2_Darwin_arm64.tar.gz"
-      sha256 "522dfa6c9115bfbb48e03f2dbb76dcd8dd82a45ba592ac74d4f6f4ac591674ff"
+      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.3/concom_0.0.3_Darwin_arm64.tar.gz"
+      sha256 "2f4ac3b7f7245bcb23915c34a36ac08443c7f36c63977e9f9218a3e717e3b080"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.2/concom_0.0.2_Linux_x86_64.tar.gz"
-      sha256 "7b22ec99f1703695e9c9a5f0a50b1831a32052ba9bc7ef2914602538f89653d2"
+      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.3/concom_0.0.3_Linux_x86_64.tar.gz"
+      sha256 "30512b487d850ca4e86dd791023ba33125581f2ad8fafe769f3d64ae040b8c4e"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.2/concom_0.0.2_Linux_arm64.tar.gz"
-      sha256 "bb436ce41118422fd3bf96a1a549932a189afb7132a6a9d09e6bc650864bc369"
+      url "https://github.com/Makepad-fr/concom/releases/download/v0.0.3/concom_0.0.3_Linux_arm64.tar.gz"
+      sha256 "4a35ff3f54a1a49e1209d72b2a354e43cccd6bc5b4d13438315dd2887b275515"
     end
   end
 
